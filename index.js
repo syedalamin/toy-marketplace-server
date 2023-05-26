42af732a1efc51b3bab265b22bc21b05988e9c47
syedalamin <syedalamin2580@gmail.com>
2023-05-26 21:05:07 +0600

    toy details email

--- index.js
+++ index.js
@@ -34,23 +34,35 @@ async function run() {
     const toysCollection = client.db('actionFigure').collection('toys');
 
     //toys
-    app.get('/toys', async(req, res)=>{
+
+    app.get('/toys', async (req, res) => {
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query?.email }
+      };
+      const result = await toysCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/toys', async (req, res) => {
       const cursor = toysCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/toys/:id', async(req, res)=> {
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id) }
-          const option = {
-            projection: { photo: 1, name: 1, seller: 1, email: 1, price: 1,  rating: 1, quantity: 1, description: 1,  }
-          }
-          const result = await toysCollection.findOne(query, option);
-          res.send(result);
+
+
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const option = {
+        projection: { photo: 1, name: 1, seller: 1, email: 1, price: 1, rating: 1, quantity: 1, description: 1, }
+      }
+      const result = await toysCollection.findOne(query, option);
+      res.send(result);
     });
 
-    app.post('/toys', async(req, res) => {
+    app.post('/toys', async (req, res) => {
       const toys = req.body;
       const result = await toysCollection.insertOne(toys);
       res.send(result);
@@ -74,12 +86,12 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('action figure toy running')
+app.get('/', (req, res) => {
+  res.send('action figure toy running')
 });
 
-app.listen(port, ()=>{
-    console.log(`action figure toy running server port: ${port}`)
+app.listen(port, () => {
+  console.log(`action figure toy running server port: ${port}`)
 });
 
 
